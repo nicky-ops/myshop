999fd435cbadb22390f7ea383434536fe157144e
Nickson Kipruto Rop <nickyrutto@gmail.com>
2024-09-27 22:36:13 +0300

    imlementing order system

--- myshop/urls.py
+++ myshop/urls.py
@@ -23,6 +23,7 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("cart/", include("cart.urls", namespace="cart")),
+    path('orders/', include('orders.urls', namespace='orders')),
     path('', include('shop.urls', namespace='shop')),
     path("__reload__/", include("django_browser_reload.urls")),
 ]
