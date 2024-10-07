d3ff7fe6488ad5586895b81f5cdb0e9646827baa
Nickson Kipruto Rop <nickyrutto@gmail.com>
2024-10-07 18:36:01 +0300

    views

--- orders/views.py
+++ orders/views.py
@@ -2,6 +2,7 @@ from cart.cart import Cart
 from django.shortcuts import render
 from .forms import OrderCreateForm
 from .models import OrderItem
+from .tasks import order_created
 
 
 
@@ -21,6 +22,8 @@ def order_create(request):
                 )
             # clear the cart
             cart.clear()
+            # launch asynchronous task
+            order_created.delay(order.id)
             return render(
                 request, 'orders/order/created.html', {'order': order}
             )
